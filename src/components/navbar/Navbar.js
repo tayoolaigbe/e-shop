160000ca44305ace8eda5d5500e032a16d35b4b9
Eyitayo Olaigbe <tayoolaigbe@outlook.com>
2021-07-01 20:05:32 +0100

    Finished Products mock Up

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+	AppBar,
+	Toolbar,
+	IconButton,
+	Badge,
+	MenuItem,
+	Menu,
+	Typography,
+} from '@material-ui/core';
+const Navbar = () => {
+	return <div></div>;
+};
+
+export default Navbar;
